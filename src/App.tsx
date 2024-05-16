55febd69ad090512e32fed2866c5551009fc6ebe
Corey Cordwell <coreyluke.cordwell@gmail.com>
2024-05-16 19:35:35 +1000

    Update text

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Programming is fun!
         </a>
       </header>
     </div>
